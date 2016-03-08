b629d650348af8292530c68b2d12c1ffb5785d17
Filip Medak <filip.medak213@gmail.com>
2016-03-08 18:25:32 +0100

    Radili smo mnogo.

--- index.php
+++ index.php
@@ -5,7 +5,24 @@
     <title>Naslov</title>
 </head>
 <body>
-    <h1>Prvi</h1>
+<h1>Prvi</h1>
+
+<p>
+
+<form>
+    Ime:<br>
+    <input type="text" name="ime"><br>
+    Prezime:<br>
+    <input type="text" name="prezime"><br>
+    <input type="submit" value="Idu dani!">
+</form>
+
+<?php
+echo('Vaše ime je: ' . htmlspecialchars($_GET['ime']) . '<br>');
+echo('Vaše prezime je: ' .$_GET['prezime']);
+?>
+
+</p>
 
 </body>
 </html>
