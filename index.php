b484c8bcf40915284fd592001cc41bc0eea32861
Filip Medak <filip.medak213@gmail.com>
2016-03-08 17:33:45 +0100

    Procistili smo PHP-ov ANU$.

--- index.php
+++ index.php
@@ -5,21 +5,7 @@
     <title>Naslov</title>
 </head>
 <body>
-<h1>Prvi</h1>
+    <h1>Prvi</h1>
 
-<p>
-    <?php
-    echo("Pozrdav iz t0p c0cka");
-    echo("<br>");
-    echo(date("d.m.Y."));
-    ?>
-</p>
-
-<p>
-    <?php
-    $dana_do_praznika = 45;
-    echo('Do praznika je ostalo jos: <b> ' . $dana_do_praznika .'</b>');
-    ?>
-</p>
 </body>
 </html>
