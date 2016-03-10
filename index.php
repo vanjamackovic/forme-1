786b2e44ff79feec204cb3bc7fde9915f734779d
vanjamackovic <vanja.mackovic@skole.hr>
2016-03-10 13:54:58 +0100

    Nova datoteka i par izmjena

--- index.php
+++ index.php
@@ -1,25 +1,43 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <link rel="stylesheet" type="text/css" href="stil.css">
     <meta charset="UTF-8">
-    <title>Naslov</title>
+    <title>Upitnik</title>
 </head>
 <body>
-<h1>Prvi</h1>
+<h1>Upitnik</h1>
 
 <p>
 
-<form>
+<form method="post">
     Ime:<br>
     <input type="text" name="ime"><br>
     Prezime:<br>
     <input type="text" name="prezime"><br>
-    <input type="submit" value="Idu dani!">
+    Spol:<br>
+    <input type="radio" name="spol" value="M">Muško<br>
+    <input type="radio" name="spol" value="Ž">Žensko<br>
+    <input type="submit" value="Pošalji!">
+
 </form>
 
 <?php
-echo('Vaše ime je: ' . htmlspecialchars($_GET['ime']) . '<br>');
-echo('Vaše prezime je: ' .$_GET['prezime']);
+echo('<br>');
+if($_POST['spol'] == 'M')
+{
+    echo('Vaše ime je: ' . htmlspecialchars($_POST['ime']) . '<br>');
+    echo('Vaše prezime je: ' . htmlspecialchars($_POST['prezime']) . '<br>');
+    echo('Vaš spol je: ' . htmlspecialchars($_POST['spol']) . '<br>');
+    echo('Vi ste muškarac!');
+}
+elseif($_POST['spol'] == 'Ž')
+{
+    echo('Vaše ime je: ' . htmlspecialchars($_POST['ime']) . '<br>');
+    echo('Vaše prezime je: ' . htmlspecialchars($_POST['prezime']) . '<br>');
+    echo('Vaš spol je: ' . htmlspecialchars($_POST['spol']) . '<br>');
+    echo('Vi ste žena!');
+}
 ?>
 
 </p>
